f71d712ce1f4a99e6d806896f4b792af21a71327
Federico Lolli <federico123579@gmail.com>
2024-05-22 18:23:58 +0200

    Add export binary in sqlite database

---
+++ qcs-host/qcs-bins/src/bin/compute.rs
@@ -0,0 +1,50 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use qcs_circuit_parser::parse_program;
+use qcs_core::{
+    contractions::{TensorKind, TensorNetwork},
+    executor::CpuExecutor,
+    model::{gates::QuantumGate, QRegister, Qubit, TensorProduct},
+    scheduler::ContractionPlan,
+};
+
+#[derive(Debug, Clone, Parser)]
+struct Cli {
+    input: PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+    let circuit = parse_program(args.input).unwrap();
+
+    let tensor_net = TensorNetwork::from(circuit.clone());
+    let contracted_nodes = tensor_net.contract().into_iter();
+
+    let mut blocks = Vec::new();
+
+    for node in contracted_nodes {
+        match node {
+            TensorKind::Contraction(contr) => {
+                let plan = ContractionPlan::from(*contr);
+                println!("{}", &plan);
+                let exec = CpuExecutor::new();
+                let start = std::time::Instant::now();
+                blocks.extend(exec.execute(plan));
+                println!("Time: {:?}", start.elapsed());
+            }
+            TensorKind::Gate(g) => blocks.push((*g).spanned_block()),
+        }
+    }
+
+    let eval = blocks.into_iter().fold(None, |acc, block| match acc {
+        None => Some(block),
+        Some(acc) => Some(acc.tensor_product(block)),
+    });
+
+    let inr = QRegister::from((0..circuit.n_qubits).map(|_| Qubit::zero()));
+    if let Some(eval) = eval {
+        let qstate_2 = eval.into_block() * inr;
+        println!("{}", qstate_2.distr());
+    }
+}
---
+++ qcs-host/schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS `programs` (
+    `id` INTEGER,
+    `filename` TEXT NOT NULL,
+    `text` TEXT NOT NULL,
+    PRIMARY KEY(`id`)
+);
+CREATE TABLE IF NOT EXISTS `contractions` (
+    `id` INTEGER,
+    `program_id` INTEGER NOT NULL,
+    `span` TEXT NOT NULL,
+    `left_id` INTEGER DEFAULT NULL,
+    `right_id` INTEGER DEFAULT NULL,
+    `kind` TEXT NOT NULL,
+    `gate_id` INTEGER DEFAULT NULL,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY(`program_id`) REFERENCES `programs`(`id`),
+    FOREIGN KEY(`left_id`, 'right_id') REFERENCES `contractions`(`id`, `id`),
+    CHECK(
+        (
+            kind = 'C'
+            AND left_id IS NOT NULL
+            AND right_id IS NOT NULL
+            AND gate_id IS NULL
+        )
+        OR (
+            kind = 'G'
+            AND left_id IS NULL
+            AND right_id IS NULL
+            AND gate_id IS NOT NULL
+        )
+    )
+);
+CREATE TABLE IF NOT EXISTS `gates` (
+    `id` INTEGER,
+    `name` TEXT NOT NULL UNIQUE,
+    `rank` INTEGER NOT NULL,
+    -- 2^(2*rank) numbers stored as a binary sequence (R1: 4, R2: 16, R3: 64, ...)
+    -- all complex numbers are stored as two 64-bit floats (real, imag) (little endian)
+    `data` BLOB NOT NULL,
+    PRIMARY KEY(`id`)
+);
