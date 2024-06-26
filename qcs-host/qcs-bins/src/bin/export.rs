66d2aaa39a6c0ab6b3e854c1df35bc3f150686fb
Federico Lolli <federico123579@gmail.com>
2024-06-26 17:46:30 +0200

    update with logging and a new option for skipping db insertions

--- qcs-host/qcs-bins/src/bin/export.rs
+++ qcs-host/qcs-bins/src/bin/export.rs
@@ -17,6 +17,8 @@ use qcs_core::{
     scheduler::ContractionPlan,
 };
 use rusqlite::{Connection, Result};
+use tracing::{debug, info, instrument, trace};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[derive(Debug, Clone, Parser)]
 struct Cli {
@@ -27,17 +29,30 @@ struct Cli {
     /// Output SQLite database
     #[clap(short, long, default_value = "db.sqlite")]
     output: String,
+
+    /// Skip inserting contractions into the database
+    #[clap(short, long, default_value = "false")]
+    skip_insert: bool,
 }
 
 const SCHEMA: &str = include_str!("../../../schema.sql");
 
 fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    debug!("Starting export");
+
     let args = Cli::parse();
     let conn = Connection::open(args.output)?;
+    debug!("Connected to database");
 
     conn.execute_batch(SCHEMA)?;
 
     for input in args.input {
+        let _outer_span = tracing::info_span!("", ?input).entered();
         let program_id = conn.insert_program(&input)?;
         let circuit = parse_program(input).unwrap();
 
@@ -52,11 +67,26 @@ fn main() -> Result<()> {
         ];
 
         let tensor_net = TensorNetwork::from(circuit.clone());
+        debug!("Starting building tensor network");
         let start = std::time::Instant::now();
         let contracted_nodes: IntoIter<TensorKind> = tensor_net.contract().into_iter();
         let tree_building_time = start.elapsed();
-        for node in contracted_nodes.clone() {
-            conn.insert_contraction(node, program_id)?;
+        info!(
+            time_us = tree_building_time.as_micros(),
+            "Finished building tensor network"
+        );
+
+        if !args.skip_insert {
+            debug!("Inserting contractions into database");
+            let start = std::time::Instant::now();
+            for node in contracted_nodes.clone() {
+                conn.insert_contraction(node, program_id)?;
+            }
+            let insertion_time = start.elapsed();
+            info!(
+                time_us = insertion_time.as_micros(),
+                "Finished inserting contractions"
+            );
         }
 
         let mut blocks = Vec::new();
@@ -66,26 +96,31 @@ fn main() -> Result<()> {
                 TensorKind::Contraction(contr) => {
                     let plan = ContractionPlan::from(*contr);
                     let exec = CpuExecutor::new();
+                    debug!("Starting contraction");
                     let start = std::time::Instant::now();
                     blocks.extend(exec.execute(plan));
                     cpu_time += start.elapsed();
+                    info!(time_us = cpu_time.as_micros(), "Finished contraction");
                 }
                 TensorKind::Gate(g) => blocks.push((*g).spanned_block()),
             }
         }
 
         // update the program with the contraction time
+        debug!("Updating program with contraction time");
         conn.update_contraction_time(
             program_id,
             cpu_time.as_micros() as u64,
             tree_building_time.as_micros() as u64,
         )?;
 
+        debug!("Combining individual blocks into a single block");
         let eval = blocks.into_iter().fold(None, |acc, block| match acc {
             None => Some(block),
             Some(acc) => Some(acc.tensor_product(block)),
         });
 
+        debug!("Starting measuring experiment time (input -> output)");
         if let Some(eval) = eval {
             let block = eval.into_block();
             let outputs = inputs
@@ -96,6 +131,8 @@ fn main() -> Result<()> {
                 conn.insert_experiment(input, output, program_id)?;
             }
         }
+
+        info!("Finished export");
     }
 
     Ok(())
@@ -152,6 +189,7 @@ trait QuantumDB {
 }
 
 impl QuantumDB for Connection {
+    #[instrument(skip(self), level = "debug", fields(gate = %gate.to_string(), rank = gate.rank()))]
     fn insert_gate(&self, gate: Gate) -> Result<i64> {
         let name = gate.to_string();
         // check first if the gate is already in the database
@@ -182,6 +220,7 @@ impl QuantumDB for Connection {
                     (&program_id, &span.to_string(), "G", &gate_id),
                 )?;
                 let id = self.last_insert_rowid();
+                trace!(program_id, "Inserted contraction");
                 Ok(id)
             }
             TensorKind::Contraction(contr) => {
@@ -193,11 +232,13 @@ impl QuantumDB for Connection {
                     (&program_id, &span.to_string(), "C", &left_id, &right_id),
                 )?;
                 let id = self.last_insert_rowid();
+                trace!(program_id, "Inserted contraction");
                 Ok(id)
             }
         }
     }
 
+    #[instrument(skip(self), level = "debug")]
     fn insert_program(&self, program: &Path) -> Result<i64> {
         let filename = program.file_name().unwrap().to_str().unwrap();
         let text = std::fs::read_to_string(program).unwrap();
@@ -205,10 +246,12 @@ impl QuantumDB for Connection {
             "INSERT INTO programs (filename, text) VALUES (?1, ?2)",
             (&filename, &text),
         )?;
+        trace!("Inserted program");
         let id = self.last_insert_rowid();
         Ok(id)
     }
 
+    #[instrument(skip(self), level = "debug")]
     fn update_contraction_time(
         &self,
         id: i64,
@@ -219,9 +262,11 @@ impl QuantumDB for Connection {
             "UPDATE programs SET contraction_cpu_time_us = ?1, tree_building_time_us = ?2 WHERE id = ?3",
             (&contraction_time_us, &tree_building_time_us, &id),
         )?;
+        trace!("Updated program with contraction time");
         Ok(())
     }
 
+    #[instrument(skip(self), level = "debug", fields(input = %input.qubits.len(), output = %output.qubits.len()))]
     fn insert_experiment(
         &self,
         input: QRegister,
@@ -234,6 +279,7 @@ impl QuantumDB for Connection {
             "INSERT INTO experiments (program_id, input_vector, output_vector) VALUES (?1, ?2, ?3)",
             (&program_id, &input, &output),
         )?;
+        trace!("Inserted experiment");
         let id = self.last_insert_rowid();
         Ok(id)
     }
