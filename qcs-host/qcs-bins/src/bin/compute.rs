3874405950f81094a6198b179bd602a72a6587c0
Federico Lolli <federico123579@gmail.com>
2024-06-30 19:35:44 +0200

    [bins] updated compute to generate csv with data

--- qcs-host/qcs-bins/src/bin/compute.rs
+++ qcs-host/qcs-bins/src/bin/compute.rs
@@ -1,11 +1,11 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, time::Duration};
 
 use clap::Parser;
 use qcs_circuit_parser::parse_program;
 use qcs_core::{
     contractions::{TensorKind, TensorNetwork},
     executor::CpuExecutor,
-    model::{gates::QuantumGate, QRegister, Qubit, TensorProduct},
+    model::{gates::QuantumGate, TensorProduct},
     scheduler::ContractionPlan,
 };
 
@@ -16,36 +16,66 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let circuit = parse_program(args.input).unwrap();
+    let circuit = parse_program(args.input.clone()).unwrap();
+
+    // if results.csv does not exist, create it and add the header
+    if !std::path::Path::new("results.csv").exists() {
+        let mut csv = csv::Writer::from_path("results.csv").unwrap();
+        csv.write_record([
+            "circuit",
+            "contraction_ns",
+            "contracted_exe_ns",
+            "normal_exe_ns",
+        ])
+        .unwrap();
+    }
+
+    // open the file in append mode
+    let csv_file = std::fs::File::options()
+        .append(true)
+        .open("results.csv")
+        .unwrap();
+    let mut csv = csv::WriterBuilder::new().from_writer(std::io::BufWriter::new(csv_file));
 
     let tensor_net = TensorNetwork::from(circuit.clone());
-    println!("Tensor Network:\n{}", tensor_net);
+    let start = std::time::Instant::now();
     let contracted_nodes = tensor_net.contract().into_iter();
+    let contracted_elapsed = start.elapsed();
 
     let mut blocks = Vec::new();
 
+    let mut contracted_exe_elapsed = Duration::new(0, 0);
     for node in contracted_nodes {
         match node {
             TensorKind::Contraction(contr) => {
                 let plan = ContractionPlan::from(*contr);
-                println!("Contraction plan:\n{}", &plan);
                 let exec = CpuExecutor::new();
                 let start = std::time::Instant::now();
                 blocks.extend(exec.execute(plan));
-                println!("CPU Execution Time: {:?}", start.elapsed());
+                contracted_exe_elapsed += start.elapsed();
             }
             TensorKind::Gate(g) => blocks.push((*g).spanned_block()),
         }
     }
 
-    let eval = blocks.into_iter().fold(None, |acc, block| match acc {
+    let start = std::time::Instant::now();
+    let _ = blocks.into_iter().fold(None, |acc, block| match acc {
         None => Some(block),
         Some(acc) => Some(acc.tensor_product(block)),
     });
+    contracted_exe_elapsed += start.elapsed();
 
-    let inr = QRegister::from((0..circuit.n_qubits).map(|_| Qubit::zero()));
-    if let Some(eval) = eval {
-        let qstate_2 = eval.into_block() * inr;
-        println!("{}", qstate_2.distr());
-    }
+    let start = std::time::Instant::now();
+    let _ = circuit.clone().eval();
+    let normal_elapsed = start.elapsed();
+
+    csv.write_record(&[
+        args.input.to_string_lossy().to_string(),
+        contracted_elapsed.as_nanos().to_string(),
+        contracted_exe_elapsed.as_nanos().to_string(),
+        normal_elapsed.as_nanos().to_string(),
+    ])
+    .unwrap();
+
+    println!("{} computed", args.input.to_string_lossy());
 }
